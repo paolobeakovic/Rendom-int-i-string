e8a530819529ae3d419ae10b2d9e06a177a9c2e6
Paolo <55134717+paolobeakovic@users.noreply.github.com>
2020-05-26 11:51:32 +0200

    Add files via upload

---
+++ rendomBroj/rendomBroj/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rendomBroj
+{
+    class Program
+{
+    static void Main(string[] args)
+    {
+        RandomGenerator generator = new RandomGenerator();
+        int rand = generator.RandomNumber(5, 100);
+        Console.WriteLine($"Random number between 5 and 100 is {rand}");
+
+        string str = generator.RandomString(10, false);
+        Console.WriteLine($"Random string of 10 chars is {str}");
+
+        string pass = generator.RandomPassword();
+        Console.WriteLine($"Random string of 6 chars is {pass}");
+
+        Console.ReadKey();
+    }
+}
+public class RandomGenerator
+{
+    // Generate a random number between two numbers    
+    public int RandomNumber(int min, int max)
+    {
+        Random random = new Random();
+        return random.Next(min, max);
+    }
+
+    // Generate a random string with a given size    
+    public string RandomString(int size, bool lowerCase)
+    {
+        StringBuilder builder = new StringBuilder();
+        Random random = new Random();
+        char ch;
+        for (int i = 0; i < size; i++)
+        {
+            ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+            builder.Append(ch);
+        }
+        if (lowerCase)
+            return builder.ToString().ToLower();
+        return builder.ToString();
+    }
+
+    // Generate a random password    
+    public string RandomPassword()
+    {
+        StringBuilder builder = new StringBuilder();
+        builder.Append(RandomString(4, true));
+        builder.Append(RandomNumber(1000, 9999));
+        builder.Append(RandomString(2, false));
+        return builder.ToString();
+    }
+}
+}
---
+++ rendomBroj/rendomBroj/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("rendomBroj")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("rendomBroj")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f9c87ded-4707-439f-8935-0110aa4ab615")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
